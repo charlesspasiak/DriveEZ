d26f48e6dc5b87b6d6c366dca4a815a7e55293ae
charlespasiak <charlesspasiak@gmail.com>
2023-08-02 14:43:15 +0700

    update SearchBar component

--- types/index.ts
+++ types/index.ts
@@ -1,10 +1,10 @@
-import { MouseEventHandler } from "react";
+import { MouseEventHandler } from 'react';
 
 export interface CustomButtonProps {
   title: string;
   containerStyles?: string;
   handleClick?: MouseEventHandler<HTMLButtonElement>;
-  btnType?: "button" | "submit";
+  btnType?: 'button' | 'submit';
   textStyles?: string;
   rightIcon?: string;
   isDisabled?: boolean;
@@ -28,4 +28,21 @@ export interface CarProps {
   model: string;
   transmission: string;
   year: number;
+}
+
+export interface FilterProps {
+  manufacturer: string;
+  year: number;
+  fuel: string;
+  limit: number;
+  model: string;
+}
+
+export interface OptionProps {
+  title: string;
+  value: string;
+}
+export interface CustomFilterProps {
+  title: string;
+  options: OptionProps[];
 }
--- utils/index.ts
+++ utils/index.ts
@@ -1,14 +1,19 @@
-import { CarProps } from '@/types';
+import { CarProps, FilterProps } from '@/types';
 
-export async function fetchCars() {
+export async function fetchCars(filters: FilterProps) {
+  const { manufacturer, year, model, limit, fuel } = filters;
+
   const headers = {
     'X-RapidAPI-Key': '7128a8609bmsh7acec982293bfdbp10cbb5jsn6e2de70d7416',
     'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com',
   };
 
-  const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3', {
-    headers: headers,
-  });
+  const response = await fetch(
+    `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
+    {
+      headers: headers,
+    }
+  );
 
   const result = await response.json();
 
